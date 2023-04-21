ebff2ed0859cb166c0b7e8aefab032b28ae3e4de
Afroz-Saifi <afrozsaifi810@gmail.com>
2023-04-21 19:10:13 +0530

    styled navigation

---
+++ script.js
@@ -0,0 +1,12 @@
+const menus = document.querySelectorAll(".nav-link");
+
+menus.forEach((menu) => {
+  menu.addEventListener("click", (e) => {
+    // Remove selected class from all size options
+    menus.forEach((menu) => {
+      menu.classList.remove("active_menu");
+    });
+    // Add active_menu class to the clicked size option
+    menu.classList.add("active_menu");
+  });
+});
